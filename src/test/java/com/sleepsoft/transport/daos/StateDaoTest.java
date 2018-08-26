1676b3a0cce53563d8b5957675f19fe90ab1e24c
agermenos <agermenos@gmail.com>
2018-08-26 12:21:34 -0700

    Adding State Controller (work in progress)

--- src/test/java/com/sleepsoft/transport/daos/StateDaoTest.java
+++ src/test/java/com/sleepsoft/transport/daos/StateDaoTest.java
@@ -3,6 +3,7 @@ package com.sleepsoft.transport.daos;
 import com.sleepsoft.transport.TransportApplication;
 import com.sleepsoft.transport.pojos.CountriesPOJO;
 import com.sleepsoft.transport.pojos.StatesPOJO;
+import com.sleepsoft.transport.services.CountryService;
 import com.sleepsoft.transport.services.StateService;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
@@ -26,8 +27,11 @@ public class StateDaoTest {
 
     public static String US_STATES_URL="https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States";
     private static String US_ID ="emx2v1zsqjkv1b9x2pl";
+    private static String TENANT_ID = "gmail.com";
     @Autowired
     StateService stateService;
+    @Autowired
+    CountryService countryService;
 
     @Test
     public void addStates(){
@@ -36,22 +40,28 @@ public class StateDaoTest {
             usPOJO.setCountry("United States");
             usPOJO.setCode("US");
             usPOJO.setId(US_ID);
-            Document doc = Jsoup.connect(US_STATES_URL).get();
-            Element table = doc.select("table").get(0);
-            Element t_body = table.select("tbody").get(0);
-            Elements tr = t_body.select("tr");
-            int headCount=0;
-            for (Element element:tr) {
-                if (headCount++>1) {
-                    StatesPOJO state = new StatesPOJO();
-                    Element th = element.select("th").get(0);
-                    Element a = th.select("a").get(0);
-                    state.setState(a.attr("title"));
-                    Elements tds = element.select("td");
-                    state.setCountry(usPOJO);
-                    state.setCode(tds.get(0).text());
-                    log.debug(state.toString());
-                    statesDao.save(state);
+            usPOJO.setTenantId(TENANT_ID);
+            usPOJO = countryService.createCountry(usPOJO).orElse(null);
+            if (usPOJO!=null) {
+                //usPOJO.setId(US_ID);
+                Document doc = Jsoup.connect(US_STATES_URL).get();
+                Element table = doc.select("table").get(0);
+                Element t_body = table.select("tbody").get(0);
+                Elements tr = t_body.select("tr");
+                int headCount = 0;
+                for (Element element : tr) {
+                    if (headCount++ > 1) {
+                        StatesPOJO state = new StatesPOJO();
+                        Element th = element.select("th").get(0);
+                        Element a = th.select("a").get(0);
+                        state.setState(a.attr("title"));
+                        Elements tds = element.select("td");
+                        state.setCountry(usPOJO);
+                        state.setCode(tds.get(0).text());
+                        state.setTenantId(TENANT_ID);
+                        log.debug(state.toString());
+                        statesDao.save(state);
+                    }
                 }
             }
 
